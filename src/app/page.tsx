b0c23373b350fc0f250d3d456276c7fa15e0f83e
hinzik <humphreydkwok@gmail.com>
2025-01-15 20:04:41 +0800

    added dark mode config, and theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,25 @@
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
-      <Card>
-        <CardHeader>
-          <CardHeader>My Form</CardHeader>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-4">
-          <Input />
-          <Button>Submit</Button>
-        </CardContent>
-      </Card>
-    </main>
+    <>
+      <main className="flex flex-1 flex-col items-center justify-center">
+        <Card>
+          <CardHeader>
+            <CardHeader>My Form</CardHeader>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-4">
+            <Input />
+            <Button>Submit</Button>
+          </CardContent>
+        </Card>
+      </main>
+      <footer className="self-end p-8">
+        <ThemeToggle />
+      </footer>
+    </>
   );
 }
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Moon, Sun } from "lucide-react";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+export default function ThemeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Prevent hydration mismatch by rendering a placeholder
+  if (!mounted) {
+    return (
+      <div className="h-6 w-6 rounded-md p-1 text-xl sm:text-2xl lg:text-base">
+        <Image
+          src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
+          width={16}
+          height={16}
+          sizes="15x15"
+          alt="Loading Light/Dark Toggle"
+          priority={false}
+          title="Loading Light/Dark Toggle"
+        />
+      </div>
+    );
+  }
+
+  return (
+    <button
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    >
+      {resolvedTheme === "dark" ? (
+        <Moon className="rounded-md p-1 text-3xl hover:cursor-pointer hover:bg-gray-700 lg:text-2xl" />
+      ) : (
+        <Sun className="rounded-md p-1 text-3xl hover:cursor-pointer hover:bg-slate-100 lg:text-2xl" />
+      )}
+    </button>
+  );
+}
